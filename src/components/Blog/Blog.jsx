e434c07c86fd92f2bd3f39372fa3337d5da501b9
Roy <156922911+ArunRoy404@users.noreply.github.com>
2025-04-20 21:46:03 +0600

    blog design done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import { CiBookmarkPlus } from "react-icons/ci";
+import { CiBookmarkCheck } from "react-icons/ci";
 
-const Blog = () => {
+const Blog = ({ blog }) => {
     return (
         <div>
             <div className="card bg-base-100 shadow-sm">
                 <figure>
                     <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        src={blog.cover}
                         alt="Shoes" />
                 </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div className="card-body space-y-2">
+                    <div className='flex justify-between'>
+                    <h2 className="card-title line-clamp-2 font-bold">{blog.title}</h2>
+                    <button className='hover:cursor-pointer'><CiBookmarkPlus size={28}></CiBookmarkPlus></button>
+                    </div>
+
+                    <div className='flex items-center gap-5'>
+                        <div className="avatar w-8">
+                            <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+                                <img src={blog.author_img} />
+                            </div>
+                        </div>
+
+                        <div><h2 className='text-lg'>{blog.author}</h2></div>
+                    </div>
+
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-primary">Mark as read</button>
                     </div>
                 </div>
             </div>
